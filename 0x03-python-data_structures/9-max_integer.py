ef6f47547eb0f84c8c0b265f0e2e461869cd294e
Happy Felix Chukwuma <happychukwuma@gmail>
2023-12-04 03:32:17 -0800

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if my_list == []:
+    if len(my_list) == 0:
         return 'None'
-    maxNum = 0
-    for i in range(len(my_list)):
-        if maxNum < my_list[i]:
-            maxNum = my_list[i]
-    return (maxNum)
+    else:
+        maxNum = 0
+        for i in range(len(my_list)):
+            if maxNum < my_list[i]:
+                maxNum = my_list[i]
+        return (maxNum)
