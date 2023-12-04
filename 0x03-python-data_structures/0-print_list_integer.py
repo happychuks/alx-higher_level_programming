ae2f96321ce3f963ef6bf3df17913ae6f590d9af
Happy Felix Chukwuma <happychukwuma@gmail>
2023-12-04 00:52:13 -0800

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 #  Function that prints all integers in a list separatee by new line
 
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
