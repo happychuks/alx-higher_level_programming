e46731faf87102d9dceda2d901be4d50bafacd6b
Happy Felix Chukwuma <happychukwuma@gmail>
2023-12-04 02:10:30 -0800

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_string = ''.join(char for char in my_string if char != 'cC')
+    my_string = ''.join(i for i in my_string if i != 'c' and i != 'C')
     return my_string
