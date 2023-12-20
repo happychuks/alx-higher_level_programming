06693321974495bd7f4534c5df1b55d3b1022f2e
Happy Felix Chukwuma <happychukwuma@gmail>
2023-12-19 16:23:35 -0800

    updated

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Define a MagicClass matching exactly a bytecode provided in 103-main.py"""
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
 
 import math
 
@@ -25,4 +25,4 @@ class MagicClass:
 
     def circumference(self):
         """Return The circumference of the MagicClass."""
-        return (2 * math.pi * self.__radius)i
+        return (2 * math.pi * self.__radius)
